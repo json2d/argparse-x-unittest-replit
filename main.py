8ef038d4847e112650f516fa24a99e14a19380f5
Jason Yung <json.yung@gmail.com>
2020-11-25 19:40:12 +0000

    workaround how both argparse and unittest both try to read argv

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import argparse
 import unittest
-
-
-class TestHello(unittest.TestCase):
-    MSG = "world"
-
-    def test__hello_config(self):
-        print(f'hello {self.MSG}')
-        self.assertTrue(True)
-        print(f'it worked {self.MSG}')
-
+import sys
 
 
 if __name__ == '__main__':
@@ -19,9 +10,21 @@ if __name__ == '__main__':
 
     a = p.parse_args()
 
-    # read config file, optionally for a specific context
-    if a.msg:
+    class TestHello(unittest.TestCase):
+        MSG = "world"
 
+        def test__hello_config(self):
+            print(f'hello {self.MSG}')
+            self.assertTrue(True)
+            print(f'it worked {self.MSG}')
+
+    if a.msg:
         TestHello.MSG = a.msg
+
+        # workaround how both argparse and unittest both try to read argv: 
+        # - pop off args for argparse before running unittest.main() which is when unittest reads from argv
+        # - careful w/ arg name conflict w/ ones expected by unittest
+        sys.argv.pop()
+        sys.argv.pop()
 
     unittest.main()
